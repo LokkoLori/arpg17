bef58fed146a3e730f9c0231447c535e0c09f4b3
LokkoLori <kaptas.andras@gmail.com>
2017-05-30 15:00:45 +0200

    almost full feature engine

--- script.js
+++ script.js
@@ -12,31 +12,35 @@ function clearchildren(node){
 	while (node.firstChild) {
 		node.removeChild(node.firstChild);
 	}
-}
+};
 
 function createButton(html, cssclass){
 	var iDiv = document.createElement('div');
 	iDiv.innerHTML = html;
 	iDiv.className = cssclass;
-	iDiv.style.cursor = "pointer"
+	iDiv.style.cursor = "pointer";
 	return iDiv;
-}
+};
 
 function Clickable(name, cstyle){
 	this.name = name;
 	this.button = null;
 	this.cstyle = cstyle;
 	this.setbutton(name);
-}
+};
 Clickable.prototype.setbutton = function(html){
 	this.button = createButton(html, this.cstyle);
-}
+};
 
 function Tool(name){
 	Clickable.call(this, name, "toolbutton");
 	this.descripion = "";
-	this.owned = False;
+	this.owned = false;
 	tools[name] = this;
+	var ethis = this;
+	this.button.onclick = function(){
+		actsite.usetool(ethis);
+	}
 };
 Tool.prototype = Object.create( Clickable.prototype );
 
@@ -44,72 +48,119 @@ function Optionc(name, owner, action){
 	Clickable.call(this, name, "optionbutton");
 	this.owner = owner;
 	this.action = action;
-}
+};
 Optionc.prototype = Object.create( Clickable.prototype );
 
 function Site(name){
 	Clickable.call(this, name, "sitebutton")
 	this.description = "";
-	this.img = "";
+	this.img = "images/"+name+".jpg";
 	this.options = [];
 	this.vars = {};
 	this.reachable = false;
 	sites[name] = this
+	var ethis = this
+	this.button.onclick = function(){
+		ethis.show()
+	}
+	this.toolrelays = []
+	
 };
 Site.prototype = Object.create( Clickable.prototype );
 
-Site.prototype.usetool = function(tool){
-}
 
 Site.prototype.addOption = function(name, action){
 	o = new Optionc(name, this, action);
 	o.button.onclick = action;
 	this.options.push(o);
+};
+
+Site.prototype.addToolRelay = function(toolname, action) {
+	this.toolrelays.push([toolname, action]);
 }
+
+Site.prototype.usetool = function(tool){
+	for (var relay of this.toolrelays){
+		if (relay[0] == tool.name){
+			relay[1]()
+			return;
+		}
+	}
+	printc(tool.name + " itt nem használható");
+};
 
 Site.prototype.show = function(){
 	picture.innerHTML = "<div>" + this.name + "</div><img src='" + this.img + "'/>";
+	options.clearchildren();
 	for (var o of this.options){
-		options.appendChild(o.button)
+		options.appendChild(o.button);
 	}
 	console.innerHTML = this.description;
-}
+	actsite = this;
+};
+
+var actsite = null;
 
 function refresh_ui(site){
-	clearchildren(sites)
+	if (!site){
+		if (actsite){
+			site = actsite;
+		} else {
+			site = sites["start"];
+		}
+	}
+	clearchildren(sites);
 	for (var s in sites){
-		if (sites[s].reachable){
-			places.appendChild(sites[s].button)
+		if (sites[s].reachable && sites[s] != site){
+			places.appendChild(sites[s].button);
 		}
 	}
 	clearchildren(tools)
 	for (var t in tools){
 		if (tools[t].owned){
-			tool.appendChild(tools[t].button)
+			inventory.appendChild(tools[t].button);
 		}
 	}
-	if (!site){
-		site = sites["start"]
-	}
-	site.show()
+	site.show();
 }
 
 function printc(text){
 	console.innerHTML += "<div>"+text+"</div>";
-}
+};
 
 function start(){
-	picture = document.getElementById("picture")
-	console = document.getElementById("console")
-	options = document.getElementById("options")
-	inventory = document.getElementById("inventory")
-	places = document.getElementById("places")
+	picture = document.getElementById("picture");
+	console = document.getElementById("console");
+	options = document.getElementById("options");
+	inventory = document.getElementById("inventory");
+	places = document.getElementById("places");
 	refresh_ui();
-}
+};
 
-var startsite = new Site("start")
+var startsite = new Site("start");
 
-startsite.reachable = true
-startsite.img  = "images/arok.jpg"
-startsite.description  = "helló világ"
-startsite.addOption("bumm", function(){printc("bummaa");})
+startsite.reachable = true;
+startsite.description  = "helló világ";
+startsite.addOption("bumm1", function(){printc(this.name);});
+startsite.addOption("bumm2", function(){printc(this.name);});
+
+var seconds = new Site("second");
+
+seconds.reachable = true;
+seconds.description  = "helló második világ";
+seconds.addOption("nyit", function(){
+	thirds.reachable = true;
+	refresh_ui();
+});
+
+newlontool = new Tool("newlontool");
+newlontool.owned = true;
+
+var thirds = new Site("third");
+
+thirds.reachable = false;
+thirds.description  = "helló haradik világ";
+
+thirds.addToolRelay("newlontool", function(){
+	printc("nem, a nyúl az!");
+});
